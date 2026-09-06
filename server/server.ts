599fa483309350e5e5ccf1453fa708d7fdf285a8
Shreyal Gupta <shreyall701@gmail.com>
2026-09-07 00:44:09 +0530

    added cors origin

--- server/server.ts
+++ server/server.ts
@@ -96,7 +96,7 @@ console.log('Cloudinary config check:', {
 const app = express();
  
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: ['http://localhost:5173', 'http://localhost:3000','https://thumblify-client-nine.vercel.app'],
     credentials: true
 }))
  
